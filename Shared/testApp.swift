ddff6b8cccd851762a976bf9d044616922d5f08a
sh3316 <chuh2016@gmail.com>
2022-05-04 16:27:25 +0900

    Initial Commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Seunghui Huh on 2022/05/04.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
