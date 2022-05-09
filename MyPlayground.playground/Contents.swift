4c31a591a0c882178bb22f138194b77503844981
sh3316 <chuh2016@gmail.com>
2022-05-09 16:51:57 +0900

    more practice

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,50 @@
 import UIKit
-func rollDice() ->
-Int {
-    return Int.random(in: 1...6)
-}
+import Darwin
+//func rollDice() ->
+//Int {
+//    return Int.random(in: 1...6)
+//}
+//
+//let random = rollDice()
 
-let random = rollDice()
+
+//func printhi (hi: String){
+//    print("hi \(hi)")
+//}
+//
+//printhi(hi:"chris")
+//
+//
+//let members = ["Jason", "Greg", "Tiffany"]
+//let nameHasT =  {(name:String) in
+//    return name.hasPrefix("T")
+//}
+//print(nameHasT)
+//
+//var x : Int?
+//if x != nil {
+//    print("not nil")}
+//else{
+//    print("nil")
+//}
+//
+//let possibleNumber = ","
+//let convertedNumber = Int(possibleNumber)
+
+//var serverResponseCode: Int? = 404
+//// serverResponseCode contains an actual Int value of 404
+//serverResponseCode = nil
+//print(type(of: serverResponseCode))
+//
+//if let actualNumber = Int(possibleNumber) {
+//    print("The string \"\(possibleNumber)\" has an integer value of \(actualNumber)")
+//} else {
+//    print("The string \"\(possibleNumber)\" couldn't be converted to an integer")
+//}
+//// Prints "The string "123" has an integer value of 123"
+
+let possibleString: String? = "An optional string."
+let forcedString: String = possibleString! // requires an exclamation point
+
+let assumedString: String! = "An implicitly unwrapped optional string."
+let implicitString: String = assumedString // no need for an exclamation point
