01ab5838cfd193de8c24b52cdb289f02e33951a5
sh3316 <chuh2016@gmail.com>
2022-05-17 14:30:09 +0900

    playground with basics

---
+++ iOS study/swift_basics.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+/*
+var onearray: Array<Int> = Array<Int>()
+let twoarray : [Int] = []
+
+print(onearray)
+print(twoarray)
+print(onearray==twoarray)
+
+func sendmessage(from myname: String, to yourname: String) -> String {
+    return "Hi \(yourname)! I'm \(myname)!"
+}
+print(sendmessage(from: "Chris", to: "Sean"))
+*/
+
+let optionalV : Int? = 3
