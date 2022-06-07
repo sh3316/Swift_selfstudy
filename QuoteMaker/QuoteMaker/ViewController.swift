4edf365c0067035d8d89895424528e9393cfd988
sh3316 <chuh2016@gmail.com>
2022-06-07 23:06:55 +0900

    quote button action

--- QuoteMaker/QuoteMaker/ViewController.swift
+++ QuoteMaker/QuoteMaker/ViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let quotes = [
+        Quotes(contents: "Hi", names: "name1"),
+        Quotes(contents: "Hello", names: "name2"),
+        Quotes(contents: "Bye", names: "name3"),
+        Quotes(contents: "Good", names: "name4"),
+        Quotes(contents: "Bad", names: "name5")
+    ]
+    @IBOutlet weak var UISmallView: UIView!
+    @IBAction func UIButton(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ QuoteMaker/QuoteMaker/quotes.swift
@@ -0,0 +1,13 @@
+//
+//  quotes.swift
+//  QuoteMaker
+//
+//  Created by Seunghui Huh on 2022/06/07.
+//
+
+import Foundation
+
+struct Quotes{
+    let contents:String
+    let names: String
+}
