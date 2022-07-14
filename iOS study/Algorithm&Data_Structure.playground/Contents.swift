dc10751a4c4d4529ff3bd41b7837e74273fb52ec
sh3316 <chuh2016@gmail.com>
2022-07-14 16:49:46 +0900

    linklist add, and describe function

--- iOS study/Algorithm&Data_Structure.playground/Contents.swift
+++ iOS study/Algorithm&Data_Structure.playground/Contents.swift
@@ -19,27 +19,8 @@ let y: String = "howman yle tters"
 //    }
 //}
 //
-//class NodeMgmt{
-//    var data:Int
-//    var head:Node?
-//    init(data:Int, head:Node?){
-//        self.data = data
-//    }
-//    func add(data:Int){
-//        if self.head==nil{
-//            self.head = Node(data: data)
-//        }
-//        else{
-//            var node = self.head!
-//            while (node.next != nil){
-//                node = node.next!
-//            }
-//            node.next = Node(data:data)
-//        }
-//    }
-//}
-//var linkedlist1 = NodeMgmt(data: 0, head: <#Node?#>)
-
+//
+//
 //func add(num:Int){
 //    var node = head
 //    while node.next != nil{
@@ -73,27 +54,36 @@ class Node{
 
 class NodeMgnt{
     var data:Int
-    var head:Node?
     var next:Node?
-    
-    init(data:Int, next:Int? = nil){
+
+    init(data:Int, next:Node? = nil){
         self.data = data
-        self.head = Node(data: data)
+        self.next = next
     }
-    func add(data: Int){
-        if self.head==nil{
-            self.head = Node(data: data)
+    func add(num: Int){
+        var node=self.next
+        if self.next==nil{
+            self.next = Node(data: num)
         }
         else{
-            var node = self.head
-            while (self.next != nil){
-                node = self.next
+            var count = 0
+            while (node!.next != nil&&count<10){
+                node = node!.next
+                count += 1
             }
-            node!.next = Node(data:data)
+            node!.next = Node(data: num)
+        }
+    }
+    func describe(){
+        var head:Node = Node(data : self.data,next : self.next)
+        while (head.next != nil){
+            print(head.data)
+            head = head.next!
         }
     }
 }
-var linkedlist1 = NodeMgnt(data:0)
-linkedlist1.add(data: 5)
-
-
+var linklist = NodeMgnt(data:0)
+linklist.add(num: 5)
+linklist.add(num: 15)
+linklist.add(num: 251)
+linklist.describe()
